52b03238a7b3cd74800723672ba4b46376a91399
Demin Dmitriy <demindf@gmail.com>
2021-06-26 15:19:42 +0300

    test_data: add snake in a corner positions

--- src/test_data.rs
+++ src/test_data.rs
@@ -1,68 +1,39 @@
 /*
+ turn=200
  A: health=100
  . . . . . . . . . . .
  . . . . . . . . . . .
  . . . . . . . . . . .
  . . . . . . . . . . .
  . . . . . . . . . . .
- . . . . . A . . . . .
- . . . . . a . . . . .
- . . . . . a . . . . .
  . . . . . . . . . . .
  . . . . . . . . . . .
  . . . . . . . . . . .
+ . . . . . . . . . . .
+ . a a . . . . . . . .
+ A a . . . . . . . . .
 */
-pub const SINGLE_SHORT_SNAKE_IN_THE_CENTER: &str = include_str!("test_data/single_short_snake_in_the_center.json");
+pub const BOTTOM_LEFT_CORNER: &str = include_str!("test_data/bottom_left_corner.json");
 
 /*
+ turn=202
  A: health=99
  . . . . . . . . . . .
  . . . . . . . . . . .
  . . . . . . . . . . .
- . . . . . . . . . . .
- . . . . . . . . . . .
- . . . . A a . . . . .
- . . . . . a . . . . .
- . . . . . . . . . . .
- . . . . . . . . . . .
- . . . . . . . . . . .
- . . . . . . . . . . .
-*/
-pub const SINGLE_SHORT_SNAKE_IN_THE_CENTER_LEFT: &str = include_str!("test_data/single_short_snake_in_the_center_left.json");
-
-/*
- A: health=99
- . . . . . . . . . . .
- . . . . . . . . . . .
- . . . . . . . . . . .
- . . . . . . . . . . .
- . . . . . . . . . . .
- . . . . . a A . . . .
- . . . . . a . . . . .
+ . . . . ¤ . A . . . .
+ . . . . . . a . . . .
+ . . . . . . a . ¤ . .
+ . . . . . a a . . . .
  . . . . . . . . . . .
  . . . . . . . . . . .
  . . . . . . . . . . .
  . . . . . . . . . . .
 */
-pub const SINGLE_SHORT_SNAKE_IN_THE_CENTER_RIGHT: &str = include_str!("test_data/single_short_snake_in_the_center_right.json");
+pub const FOOD_IN_FRONT_UP_UP: &str = include_str!("test_data/food_in_front_up_up.json");
 
 /*
- A: health=99
- . . . . . . . . . . .
- . . . . . . . . . . .
- . . . . . . . . . . .
- . . . . . . . . . . .
- . . . . . A . . . . .
- . . . . . a . . . . .
- . . . . . a . . . . .
- . . . . . . . . . . .
- . . . . . . . . . . .
- . . . . . . . . . . .
- . . . . . . . . . . .
-*/
-pub const SINGLE_SHORT_SNAKE_IN_THE_CENTER_UP: &str = include_str!("test_data/single_short_snake_in_the_center_up.json");
-
-/*
+ turn=200
  A: health=21
  . . . . . . . . . . .
  . . . . . . . . . . .
@@ -79,17 +50,86 @@ pub const SINGLE_SHORT_SNAKE_IN_THE_CENTER_UP: &str = include_str!("test_data/si
 pub const FOOD_IN_FRONT: &str = include_str!("test_data/food_in_front.json");
 
 /*
+ turn=201
  A: health=99
  . . . . . . . . . . .
  . . . . . . . . . . .
  . . . . . . . . . . .
- . . . . ¤ . A . . . .
- . . . . . . a . . . .
- . . . . . . a . ¤ . .
- . . . . . a a . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . A a . . . . .
+ . . . . . a . . . . .
  . . . . . . . . . . .
  . . . . . . . . . . .
  . . . . . . . . . . .
  . . . . . . . . . . .
 */
-pub const FOOD_IN_FRONT_UP_UP: &str = include_str!("test_data/food_in_front_up_up.json");
+pub const SINGLE_SHORT_SNAKE_IN_THE_CENTER_LEFT: &str = include_str!("test_data/single_short_snake_in_the_center_left.json");
+
+/*
+ turn=201
+ A: health=99
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . a A . . . .
+ . . . . . a . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+*/
+pub const SINGLE_SHORT_SNAKE_IN_THE_CENTER_RIGHT: &str = include_str!("test_data/single_short_snake_in_the_center_right.json");
+
+/*
+ turn=201
+ A: health=99
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . A . . . . .
+ . . . . . a . . . . .
+ . . . . . a . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+*/
+pub const SINGLE_SHORT_SNAKE_IN_THE_CENTER_UP: &str = include_str!("test_data/single_short_snake_in_the_center_up.json");
+
+/*
+ turn=200
+ A: health=100
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . A . . . . .
+ . . . . . a . . . . .
+ . . . . . a . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+*/
+pub const SINGLE_SHORT_SNAKE_IN_THE_CENTER: &str = include_str!("test_data/single_short_snake_in_the_center.json");
+
+/*
+ turn=200
+ A: health=100
+ . . . . . . . . a a A
+ . . . . . . . . a . .
+ . . . . . . . . a . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+ . . . . . . . . . . .
+*/
+pub const TOP_RIGHT_CORNER: &str = include_str!("test_data/top_right_corner.json");
