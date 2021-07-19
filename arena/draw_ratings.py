68242e2b9ddbbfa7eb6f828dbf28a956e1c578c8
Demin Dmitriy <demindf@gmail.com>
2021-07-19 19:36:15 +0300

    arena: implement draw_ratings.py which draws boxplot

---
+++ arena/draw_ratings.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import json
+
+
+def main():
+    ratings = json.load(open('ratings.json'))
+    if len(ratings) == 0:
+        print('No ratings')
+        return
+
+
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    boxes = [
+        {
+            'label' : name,
+            'whislo': rating['mu'] - 3 * rating['sigma'],
+            'q1'    : rating['mu'] - rating['sigma'],
+            'med'   : rating['mu'],
+            'q3'    : rating['mu'] + rating['sigma'],
+            'whishi': rating['mu'] + 3 * rating['sigma'],
+            'fliers': []
+        }
+        for name, rating in ratings.items()
+    ]
+    ax.bxp(boxes, showfliers=False)
+    ax.set_ylabel("trueskill")
+    ax.set_ylim([15, 35])
+    plt.gca().yaxis.set_major_locator(plt.MultipleLocator(5))
+    plt.gca().yaxis.set_minor_locator(plt.MultipleLocator(1))
+    ax.grid()
+    plt.savefig("ratings.png")
+    plt.close()
+
+
+if __name__ == '__main__':
+    main()
