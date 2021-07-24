56b666ff8ed03c646c99422f66e642a14125cf98
Demin Dmitriy <demindf@gmail.com>
2021-07-25 02:30:48 +0300

    arena.draw_ratings: automatically select y-range

--- arena/draw_ratings.py
+++ arena/draw_ratings.py
@@ -4,7 +4,14 @@ from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import json
 import sys
+from math import ceil, floor
 
+
+def minmax(ratings):
+    return [
+        floor(min(r['mu'] - 2 * r['sigma'] for r in ratings.values()) / 5) * 5,
+        ceil (max(r['mu'] + 2 * r['sigma'] for r in ratings.values()) / 5) * 5
+    ]
 
 def main():
     filename = 'ratings.json' if len(sys.argv) <= 1 else sys.argv[1]
@@ -31,12 +38,12 @@ def main():
     ax.bxp(boxes, showfliers=False)
     ax.set_ylabel("trueskill")
     ax.get_xaxis().set_tick_params(rotation=-90)
-    ax.set_ylim([10, 40])
+    ax.set_ylim(minmax(ratings))
     fig.tight_layout()
-    fig.subplots_adjust(bottom=0.12, top=0.97)
+    fig.subplots_adjust(bottom=0.25, top=0.97)
 
-    fig.set_size_inches(20, 15)
-    plt.gca().yaxis.set_major_locator(plt.MultipleLocator(5))
+    fig.set_size_inches(10, 10)
+    plt.gca().yaxis.set_major_locator(plt.MultipleLocator(1))
     plt.gca().yaxis.set_minor_locator(plt.MultipleLocator(1))
     ax.grid()
     plt.savefig("ratings.png")
