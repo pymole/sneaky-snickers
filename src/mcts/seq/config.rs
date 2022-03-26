4aa6fd3ed94f26aa503df0c2b48d0370c49a6087
Demin Dmitriy <demindf@gmail.com>
2022-03-26 12:49:06 +0300

    change default MCTS_ROLLOUT_CUTOFF from 21 to 0

--- src/mcts/seq/config.rs
+++ src/mcts/seq/config.rs
@@ -22,7 +22,7 @@ impl MCTSConfig for SequentialMCTSConfig {
             table_capacity: parse_env("MCTS_TABLE_CAPACITY").unwrap_or(200000),
             iterations:     parse_env("MCTS_ITERATIONS"),
             search_time:    parse_env("MCTS_SEARCH_TIME").map(Duration::from_millis),
-            rollout_cutoff: parse_env("MCTS_ROLLOUT_CUTOFF").unwrap_or(21),
+            rollout_cutoff: parse_env("MCTS_ROLLOUT_CUTOFF").unwrap_or(0),
             rollout_beta:   parse_env("MCTS_ROLLOUT_BETA").unwrap_or(3.0),
             draw_reward:    parse_env("MCTS_DRAW_REWARD").unwrap_or(NORMALIZED_DRAW_REWARD),
         };
