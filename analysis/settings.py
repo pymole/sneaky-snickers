53ac855aeb2bfbd70fccc067ae51639de3363b73
Shevela Roman <goglbummm@gmail.com>
2022-11-15 04:34:07 +0500

    feat(data): Prepare positions, xgboost

--- analysis/settings.py
+++ analysis/settings.py
@@ -2,3 +2,4 @@ import os
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://battlesnake:battlesnake@localhost:27017/battlesnake?authSource=admin")
 GAMES_COLLECTION_NAME = os.getenv("GAMES_COLLECTION_NAME", "games")
+SNAKES_COUNT = 2
