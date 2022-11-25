c1f89a35d8e04215a4eb9cbd4616be3f97d775b5
Shevela Roman <goglbummm@gmail.com>
2022-11-25 18:09:09 +0500

    refactor(board_generator): Fixed snakes spawn

--- balalaika/src/board_generator.rs
+++ balalaika/src/board_generator.rs
@@ -1,25 +1,21 @@
-use std::collections::HashSet;
 
-use crate::{game::{Board, random_point_inside_borders, Snake}};
+use arrayvec::ArrayVec;
+use rand::{
+    seq::SliceRandom,
+    thread_rng,
+    Rng,
+};
+
+use crate::game::{
+    Board,
+    Snake,
+    MAX_SNAKE_COUNT,
+    Point,
+    WIDTH,
+};
 
 pub fn generate_board() -> Board {
-    let mut heads = HashSet::new();
-    let head = random_point_inside_borders();
-    heads.insert(head);
-    while heads.len() < 4 {
-        let head = random_point_inside_borders();
-        if !heads.contains(&head) {
-            heads.insert(head);
-        }
-    }
-
-    let snakes = heads
-        .into_iter()
-        .map(|head| Snake {
-            health: 100,
-            body: [head, head, head].try_into().unwrap(),
-        })
-        .collect();
+    let snakes = make_snakes();
 
     // let foods = vec![];
     // let hazards = vec![];
@@ -32,4 +28,49 @@ pub fn generate_board() -> Board {
     );
 
     board
+}
+
+fn make_snakes() -> ArrayVec<Snake, MAX_SNAKE_COUNT> {
+    // With fixed positions
+    let rng = &mut thread_rng();
+
+	// Create start 8 points
+	let mn = 1;
+    let md = (WIDTH - 1)/2;
+    let mx = WIDTH - 2;
+
+    let mut start_points = if rng.gen_bool(0.5) {
+        let corner_points = [
+            Point {x: mn, y: mn},
+            Point {x: mn, y: mx},
+            Point {x: mx, y: mn},
+            Point {x: mx, y: mx},
+        ];
+        corner_points
+    } else {
+        let cardinal_points = [
+            Point {x: mn, y: md},
+            Point {x: md, y: mn},
+            Point {x: md, y: mx},
+            Point {x: mx, y: md},
+        ];
+        cardinal_points
+    };
+	start_points.shuffle(rng);
+	
+	// Assign to snakes in order given
+    let mut snakes = ArrayVec::new();
+    for i in 0..MAX_SNAKE_COUNT {
+        let p = start_points[i];
+        let snake = Snake {
+            health: 100,
+            body: [p, p, p].try_into().unwrap(),
+        };
+        snakes.push(snake);
+    }
+    snakes
+}
+
+fn make_food() {
+    
 }
